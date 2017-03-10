0b760117fdf4004f97e08a26c6b8a22e4519ccac
John Anderson <jander43@vols.utk.edu>
2017-03-10 07:42:22 -0500

    Updated for multiple terminals

--- cloneterm.sh
+++ cloneterm.sh
@@ -1,8 +1,16 @@
 #!/usr/bin/env bash
 
 # funcs
+open_term() {
+  open -a "$1" "$2"
+}
+
 main() {
-  open -a iTerm "$1"
+  case $TERM_PROGRAM in
+    "iTerm.app") open_term "iTerm" "$1" ;;
+    "Apple_Terminal") open_term "Terminal" "$1" ;;
+    "Hyper") open_term "Hyper" "$1" ;;
+  esac
 }
 
 # run main
